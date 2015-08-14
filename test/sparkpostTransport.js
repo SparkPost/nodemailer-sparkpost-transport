03210beb0d80d7f93ec76cccd1db575095548bd7
Benjamin Dean <ben@benjamindean.com>
2015-08-14 11:47:53 -0700

    WIP

--- test/sparkpostTransport.js
+++ test/sparkpostTransport.js
@@ -7,19 +7,23 @@ var sparkpostTransport = require('../');
 
 var pkg = require('../package.json');
 
-describe('SparkPostTransport', function() {
+describe('SparkPost Transport Tests', function() {
+  var transport = sparkpostTransport();
 
   it('should have a name and version property', function(done) {
-    var transport = sparkpostTransport();
     expect(transport).to.have.property('name', 'Sparkpost');
     expect(transport).to.have.property('version', pkg.version);
     done();
   });
 
   it('should expose a send method', function(done) {
-    var transport = sparkpostTransport();
     expect(transport.send).to.exist;
     expect(transport.send).to.be.a('function');
     done();
   });
+});
+
+describe('Send Method', function() {
+  // TODO: Handle exceptions
+  // Respond with appropriate data per Nodemailer spec
 });
