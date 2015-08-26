17666d94dd5261dae242b25e691250e608a04b77
Benjamin Dean <ben@benjamindean.com>
2015-08-26 14:33:50 -0700

    Tests passing...might need to add some more, but good enough to start testing in an test-harness application

--- test/sparkpostTransport.js
+++ test/sparkpostTransport.js
@@ -24,6 +24,7 @@ describe("SparkPost Transport Tests", function() {
 
   it("should be able to set options", function(done) {
     var transport = sparkpostTransport({
+      campaign_id: "sample_campaign",
       tags: ["new-account-notification"],
       metadata: {"source": "event"},
       substitution_data: {"salutatory": "Welcome to SparkPost!"},
@@ -32,6 +33,7 @@ describe("SparkPost Transport Tests", function() {
       recipients: [{"email": "john.doe@example.com", "name": "John Doe"}]
     });
 
+    expect(transport.campaign_id).to.equal("sample_campaign");
     expect(transport.tags).to.deep.equal(["new-account-notification"]);
     expect(transport.metadata).to.deep.equal({"source": "event"});
     expect(transport.substitution_data).to.deep.equal({"salutatory": "Welcome to SparkPost!"});
@@ -49,6 +51,7 @@ describe("Send Method", function() {
   it("should be able to overload options at the transmission", function(done) {
     // Create the default transport
     var transport = sparkpostTransport({
+      campaign_id: "sample_campaign",
       tags: ["new-account-notification"],
       metadata: {"source": "event"},
       substitution_data: {"salutatory": "Welcome to SparkPost!"},
@@ -57,21 +60,16 @@ describe("Send Method", function() {
       recipients: [{"email": "john.doe@example.com", "name": "John Doe"}]
     });
 
-    // Grab the SDK used for sending transmission
-    var sparkpostEmailClient = transport.sparkpostClient;
-
-    console.log( 'STOP 1' );
     // Stub the send method of the SDK out
-    var stub = sinon.stub(sparkpostEmailClient.transmissions, "send", function(data, resolve) {
-    console.log( 'STOP 2' );
+    var stub = sinon.stub(transport, "send", function(data, resolve) {
       // Grab the transmissionBody from the send() payload for assertions
-      var mail = data.transmissionBody;
-      expect(mail.tags).to.deep.equal(["alternative-tag"]);
-      expect(mail.metadata).to.deep.equal({"changedKey": "value"});
-      expect(mail.substitution_data).to.deep.equal({"salutatory": "And now...for something completely different"});
-      expect(mail.options).to.deep.equal({"click_tracking": false, "open_tracking": false, "transactional": true});
-      expect(mail.content).to.deep.equal({"template_id": "someOtherTemplate"});
-      expect(mail.recipients).to.deep.equal({"list_id": "myStoredRecipientList"});
+      expect(data.campaign_id).to.equal("another_sample_campaign");
+      expect(data.tags).to.deep.equal(["alternative-tag"]);
+      expect(data.metadata).to.deep.equal({"changedKey": "value"});
+      expect(data.substitution_data).to.deep.equal({"salutatory": "And now...for something completely different"});
+      expect(data.options).to.deep.equal({"click_tracking": false, "open_tracking": false, "transactional": true});
+      expect(data.content).to.deep.equal({"template_id": "someOtherTemplate"});
+      expect(data.recipients).to.deep.equal([{"list_id": "myStoredRecipientTestList"}]);
 
       // Resolve the stub's spy
       resolve({
@@ -81,29 +79,31 @@ describe("Send Method", function() {
           id: "66123596945797072"
         }
       });
-
-    console.log( 'STOP 3' );
-      // Create the modified options for use with the above stub test
-      var overloadedTransmission = {
-        tags: ["alternative-tag"],
-        metadata: {"changedKey": "value"},
-        substitution_data: {"salutatory": "And now...for something completely different"},
-        options: {"click_tracking": false, "open_tracking": false, "transactional": true},
-        recipients: [{
-          list_id: "myStoredRecipientTestList"
-        }],
-        content: {
-          template_id: "someOtherTemplate"
-        }
-      };
-
-    console.log( 'STOP 4' );
-      // Call the stub from above
-      transport.send(overloadedTransmission, done);
-      // Return the original method to its proper state
-      transport.send.restore();
-    console.log( 'STOP 5' );
     });
 
+    // Create the modified options for use with the above stub test
+    var overloadedTransmission = {
+      campaign_id: "another_sample_campaign",
+      tags: ["alternative-tag"],
+      metadata: {"changedKey": "value"},
+      substitution_data: {"salutatory": "And now...for something completely different"},
+      options: {"click_tracking": false, "open_tracking": false, "transactional": true},
+      recipients: [{
+        list_id: "myStoredRecipientTestList"
+      }],
+      content: {
+        template_id: "someOtherTemplate"
+      }
+    };
+
+    // Call the stub from above
+    transport.send(overloadedTransmission, function(data) {
+      expect(data.results.id).to.exist;
+      expect(data.results.total_rejected_recipients).to.exist;
+      expect(data.results.total_accepted_recipients).to.exist;
+      done();
+    });
+    // Return the original method to its proper state
+    transport.send.restore();
   });
 });
